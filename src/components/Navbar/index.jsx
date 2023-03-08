0ea6200b9a21f766a0f0207f6dfd7ea361e638da
Prince Neres <prince84224960@gmail.com>
2023-03-08 20:47:56 -0300

    Fix style navbar with tailwind

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,25 +1,27 @@
 import { Link } from 'react-router-dom';
+import { MoonIcon } from '@heroicons/react/24/solid';
 
 export default function Navbar () {
   return (
-    <nav>
-      <div>
-        <div className={'link'}>
+    <nav className="flex items-center h-20 bg-gray-500 justify-between px-10 sm:rounded-xl sm:m-5">
+			<div className="flex items-center">
+        <div className="text-gray-100 px-5">
           <Link to='/'>
             Clima local
           </Link>
         </div>
-        <div className={'link'}>
+        <div className="text-gray-100 px-5">
           <Link to='/contact'>
             Contato
           </Link>
         </div>
-        <div className={'link'}>
+        <div className="text-gray-100 px-5">
           <Link to='/search_cep'>
             Busca CEP
           </Link>
         </div>
-      </div>
+			</div>
+			<MoonIcon className="h-14 text-gray-100 hidden sm:block cursor-pointer"/>
     </nav>
   )
 };
