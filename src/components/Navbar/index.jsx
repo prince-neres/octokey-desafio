6da01a65eb9d49e807d28abd3e657fadd9a949b9
Prince Neres <prince84224960@gmail.com>
2023-03-15 17:24:16 -0300

    Fix navbar responsive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,8 +5,8 @@ export default function Navbar () {
 	const location = useLocation();
 
   return (
-    <nav className="shadow-lg flex items-center h-20 bg-light-200 dark:bg-dark-200 justify-between px-10 sm:rounded-xl sm:m-5 sm:px-5">
-			<div className="flex items-center">
+    <nav className="shadow-lg flex items-center h-20 bg-light-200 dark:bg-dark-200 px-10 justify-center sm:justify-between sm:rounded-xl sm:m-5 sm:px-5">
+			<div className="flex items-center flex-col sm:flex-row">
         <div className="px-5">
           <Link to="/" className={location.pathname === "/" ? "font-bold" : ""}>
             Clima local
